f4992286c3503228574a1a2ad868822ca7c5bc29
Tyler Hancock <darklime@live.ca>
2017-09-04 16:03:40 -0600

    Cleanup and formatting.

--- src/main/java/net/darkhax/tinkerstages/commands/CommandTconDump.java
+++ src/main/java/net/darkhax/tinkerstages/commands/CommandTconDump.java
@@ -13,47 +13,47 @@ public class CommandTconDump extends Command {
 
     @Override
     public String getName () {
-        
+
         return "tcondump";
     }
 
     @Override
     public String getUsage (ICommandSender sender) {
-        
+
         return "tcondump materials|modifiers";
     }
 
     @Override
     public void execute (MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        
+
         if (args.length == 1) {
-            
+
             if ("materials".equalsIgnoreCase(args[0])) {
-                
-                for (Material material : TinkerRegistry.getAllMaterials()) {
-                    
+
+                for (final Material material : TinkerRegistry.getAllMaterials()) {
+
                     sender.sendMessage(new TextComponentString("Name: " + material.getLocalizedName() + " Id: " + material.getIdentifier()));
                 }
             }
-            
+
             else if ("modifiers".equalsIgnoreCase(args[0])) {
-                
-                for (IModifier modifier : TinkerRegistry.getAllModifiers()) {
-                    
+
+                for (final IModifier modifier : TinkerRegistry.getAllModifiers()) {
+
                     sender.sendMessage(new TextComponentString("Name: " + modifier.getLocalizedName() + " Id: " + modifier.getIdentifier()));
                 }
             }
         }
-        
+
         else {
-            
+
             sender.sendMessage(new TextComponentString(this.getUsage(sender)));
         }
     }
-    
+
     @Override
-    public int getRequiredPermissionLevel() {
-        
+    public int getRequiredPermissionLevel () {
+
         return 0;
     }
 }
