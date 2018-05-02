0f176aa31c996905cf444d40ee2f5c97b32e310d
Tyler Hancock <darklime@live.ca>
2018-05-01 20:59:58 -0600

    Updated to GameStages 2.0

--- src/main/java/net/darkhax/tinkerstages/TinkerStages.java
+++ src/main/java/net/darkhax/tinkerstages/TinkerStages.java
@@ -7,11 +7,10 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
 import net.darkhax.bookshelf.lib.LoggingHelper;
-import net.darkhax.bookshelf.util.PlayerUtils;
 import net.darkhax.bookshelf.util.StackUtils;
+import net.darkhax.gamestages.GameStageHelper;
 import net.darkhax.gamestages.GameStages;
-import net.darkhax.gamestages.capabilities.PlayerDataHandler;
-import net.darkhax.gamestages.capabilities.PlayerDataHandler.IStageData;
+import net.darkhax.gamestages.data.IStageData;
 import net.darkhax.tinkerstages.commands.CommandTconDump;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -19,8 +18,6 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import slimeknights.tconstruct.library.events.TinkerCraftingEvent.ToolCraftingEvent;
@@ -32,237 +29,202 @@ import slimeknights.tconstruct.library.modifiers.IModifier;
 import slimeknights.tconstruct.library.utils.TagUtil;
 import slimeknights.tconstruct.library.utils.TinkerUtil;
 
-@Mod(modid = "tinkerstages", name = "Tinker Stages", version = "@VERSION@", dependencies = "required-after:tconstruct@[1.12-2.7.2.27,);required-after:bookshelf@[2.1.443,);required-after:gamestages@[1.0.63,);required-after:crafttweaker@[2.7.2.,)", certificateFingerprint = "@FINGERPRINT@")
+@Mod(modid = "tinkerstages", name = "Tinker Stages", version = "@VERSION@", dependencies = "required-after:tconstruct;required-after:bookshelf;required-after:gamestages@[2.0.89,);required-after:crafttweaker", certificateFingerprint = "@FINGERPRINT@")
 public class TinkerStages {
-
+    
     /**
-     * The logger for the mod. 
+     * The logger for the mod.
      */
     public static final LoggingHelper LOG = new LoggingHelper("Tinker Stages");
-
+    
     /**
      * A set of stages for being able to craft tinker tools. At least one of these stages must
      * be unlocked for tinker tool crafting to be available.
      */
     public static Set<String> GENERAL_CRAFTING_STAGES = new HashSet<>();
-
+    
     /**
      * A set of stages for being able to replace tinker tool parts. At least one of these
      * stages must be unlocked for part replacing to be available.
      */
     public static Set<String> GENERAL_PART_REPLACING_STAGES = new HashSet<>();
-
+    
     /**
      * A set of stages for being able to create tinker tool parts. At least one of these stages
      * must be unlocked for part making to be available.
      */
     public static Set<String> GENERAL_PART_BUILDING_STAGES = new HashSet<>();
-
+    
     /**
      * A set of stages for being able to apply tool modifiers. At least one of these stages
      * must be unlocked for applying modifiers to be available.
      */
     public static Set<String> GENERAL_MODIFIER_STAGES = new HashSet<>();
-
+    
     /**
      * A map of possible stages needed to craft a specific tool type. Only one of the stages is
      * needed to craft that tool type.
      */
     public static Multimap<String, String> TOOL_CRAFTING_STAGES = HashMultimap.create();
-
+    
     /**
      * A map of possible stages needed to use a specific tool material. Only one of the stages
      * is needed to use the material.
      */
     public static Multimap<String, String> TOOL_MATERIAL_STAGES = HashMultimap.create();
-
+    
     /**
      * A map of possible stages needed to apply a specific tool modifier. Only one of the
      * stages is needed to apply the modifier.
      */
     public static Multimap<String, String> TOOL_MODIFIER_STAGES = HashMultimap.create();
-
+    
     @Mod.EventHandler
     public void preInit (FMLPreInitializationEvent event) {
-
+        
         MinecraftForge.EVENT_BUS.register(this);
         GameStages.COMMAND.addSubcommand(new CommandTconDump());
     }
-
+    
     @SubscribeEvent
     public void onToolCrafted (ToolCraftingEvent event) {
-
-        final IStageData stageData = PlayerDataHandler.getStageData(event.getPlayer());
+        
         final String itemId = StackUtils.getStackIdentifier(event.getItemStack());
-
-        // Bad stage data
-        if (stageData == null) {
-
-            event.setCanceled("You have no GameStage data! This shouldn't be possible!");
-            return;
-        }
-
+        
         // General crafting prevention
-        if (!GENERAL_CRAFTING_STAGES.isEmpty() && !stageData.hasUnlockedAnyOf(GENERAL_CRAFTING_STAGES)) {
-
-            System.out.print("fuck");
+        if (!GENERAL_CRAFTING_STAGES.isEmpty() && !GameStageHelper.hasAnyOf(event.getPlayer(), GENERAL_CRAFTING_STAGES)) {
+            
             event.setCanceled("You can not craft any Tinkers Construct tools at this time. Further progression is needed.");
             return;
         }
-
+        
         // Prevent specific tool crafting
-        if (TOOL_CRAFTING_STAGES.containsKey(itemId) && !stageData.hasUnlockedAnyOf(TOOL_CRAFTING_STAGES.get(itemId))) {
-
+        if (TOOL_CRAFTING_STAGES.containsKey(itemId) && !GameStageHelper.hasAnyOf(event.getPlayer(), TOOL_CRAFTING_STAGES.get(itemId))) {
+            
             event.setCanceled("You can not craft the " + event.getItemStack().getDisplayName() + " at this time. Further progression is needed.");
             return;
         }
-
+        
         // Specific material prevention
         for (final ItemStack part : event.getToolParts()) {
-
+            
             if (!part.isEmpty()) {
-
+                
                 final Material material = TinkerUtil.getMaterialFromStack(part);
-
-                if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !stageData.hasUnlockedAnyOf(TOOL_MATERIAL_STAGES.get(material.identifier))) {
-
+                
+                if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !GameStageHelper.hasAnyOf(event.getPlayer(), TOOL_MATERIAL_STAGES.get(material.identifier))) {
+                    
                     event.setCanceled("You can not use the " + part.getDisplayName() + " in tools yet. Further progression is needed for " + material.getLocalizedName() + " tools.");
                     return;
                 }
             }
         }
     }
-
+    
     @SubscribeEvent
     public void onToolPartSwap (ToolPartReplaceEvent event) {
-
-        final IStageData stageData = PlayerDataHandler.getStageData(event.getPlayer());
-
-        // Bad stage data
-        if (stageData == null) {
-
-            event.setCanceled("You have no GameStage data! This shouldn't be possible!");
-            return;
-        }
-
+        
         // General replacing prevention
-        if (!GENERAL_PART_REPLACING_STAGES.isEmpty() && !stageData.hasUnlockedAnyOf(GENERAL_PART_REPLACING_STAGES)) {
-
+        if (!GENERAL_PART_REPLACING_STAGES.isEmpty() && !GameStageHelper.hasAnyOf(event.getPlayer(), GENERAL_PART_REPLACING_STAGES)) {
+            
             event.setCanceled("You can not swap any tool parts at this time. Further progression is needed.");
             return;
         }
-
+        
         // Specific material prevention
         for (final ItemStack part : event.getToolParts()) {
-
+            
             if (!part.isEmpty()) {
-
+                
                 final Material material = TinkerUtil.getMaterialFromStack(part);
-
-                if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !stageData.hasUnlockedAnyOf(TOOL_MATERIAL_STAGES.get(material.identifier))) {
-
+                
+                if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !GameStageHelper.hasAnyOf(event.getPlayer(), TOOL_MATERIAL_STAGES.get(material.identifier))) {
+                    
                     event.setCanceled("You can not use the " + part.getDisplayName() + " for tools yet. Further progression is needed.");
                     return;
                 }
             }
         }
     }
-
+    
     @SubscribeEvent
     public void onToolPartCrafted (ToolPartCraftingEvent event) {
-
-        final IStageData stageData = PlayerDataHandler.getStageData(event.getPlayer());
+        
         final Material material = TinkerUtil.getMaterialFromStack(event.getItemStack());
-
-        // Bad stage data
-        if (stageData == null) {
-
-            event.setCanceled("You have no GameStage data! This shouldn't be possible!");
-            return;
-        }
-
+        
         // General part building prevention
-        if (!GENERAL_PART_BUILDING_STAGES.isEmpty() && !stageData.hasUnlockedAnyOf(GENERAL_PART_BUILDING_STAGES)) {
-
+        if (!GENERAL_PART_BUILDING_STAGES.isEmpty() && !GameStageHelper.hasAnyOf(event.getPlayer(), GENERAL_PART_BUILDING_STAGES)) {
+            
             event.setCanceled("You can not build any tool parts at this time. Further progression is needed.");
             return;
         }
-
+        
         // Specific material prevention
-        if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !stageData.hasUnlockedAnyOf(TOOL_MATERIAL_STAGES.get(material.identifier))) {
-
+        if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !GameStageHelper.hasAnyOf(event.getPlayer(), TOOL_MATERIAL_STAGES.get(material.identifier))) {
+            
             event.setCanceled("You can not make parts from " + material.getLocalizedName() + " yet. Further progression is needed.");
             return;
         }
     }
-
+    
     @SubscribeEvent
     public void onToolModified (ToolModifyEvent event) {
-
-        final IStageData stageData = PlayerDataHandler.getStageData(event.getPlayer());
-
-        // Bad stage data
-        if (stageData == null) {
-
-            event.setCanceled("You have no GameStage data! This shouldn't be possible!");
-            return;
-        }
-
+        
         // General modifier prevention
-        if (!GENERAL_MODIFIER_STAGES.isEmpty() && !stageData.hasUnlockedAnyOf(GENERAL_MODIFIER_STAGES)) {
-
+        if (!GENERAL_MODIFIER_STAGES.isEmpty() && !GameStageHelper.hasAnyOf(event.getPlayer(), GENERAL_MODIFIER_STAGES)) {
+            
             event.setCanceled("You can not apply any tool modifiers at this time. Further progression is needed.");
             return;
         }
-
+        
         // Prevent specific modifier
         for (final IModifier modifier : event.getModifiers()) {
-
-            if (TOOL_MODIFIER_STAGES.containsKey(modifier.getIdentifier()) && !stageData.hasUnlockedAnyOf(TOOL_MODIFIER_STAGES.get(modifier.getIdentifier()))) {
-
+            
+            if (TOOL_MODIFIER_STAGES.containsKey(modifier.getIdentifier()) && !GameStageHelper.hasAnyOf(event.getPlayer(), TOOL_MODIFIER_STAGES.get(modifier.getIdentifier()))) {
+                
                 event.setCanceled("You can not apply the " + modifier.getLocalizedName() + " modifier at this time. Further progression is needed.");
                 return;
             }
         }
     }
-
+    
     @SubscribeEvent
     public void onLivingUpdate (LivingUpdateEvent event) {
-
-        if (PlayerUtils.isPlayerReal(event.getEntityLiving())) {
-
+        
+        if (event.getEntityLiving() instanceof EntityPlayer && !event.getEntityLiving().world.isRemote) {
+            
             final EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-            final IStageData stageData = PlayerDataHandler.getStageData(player);
+            final IStageData stageData = GameStageHelper.getPlayerData(player);
             final ItemStack stack = player.getHeldItemMainhand();
-
+            
             final String itemId = StackUtils.getStackIdentifier(stack);
-
+            
             if (stageData != null && !stack.isEmpty()) {
-
+                
                 // Prevent specific tool
-                if (TOOL_CRAFTING_STAGES.containsKey(itemId) && !stageData.hasUnlockedAnyOf(TOOL_CRAFTING_STAGES.get(itemId))) {
-
+                if (TOOL_CRAFTING_STAGES.containsKey(itemId) && !GameStageHelper.hasAnyOf(player, stageData, TOOL_CRAFTING_STAGES.get(itemId))) {
+                    
                     player.sendMessage(new TextComponentString("You dropped the " + stack.getDisplayName() + "! Further progression is needed to wield this type of tool."));
                     player.dropItem(true);
                     return;
                 }
-
+                
                 // Specific material prevention
                 for (final Material material : TinkerUtil.getMaterialsFromTagList(TagUtil.getBaseMaterialsTagList(stack))) {
-
-                    if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !stageData.hasUnlockedAnyOf(TOOL_MATERIAL_STAGES.get(material.identifier))) {
-
+                    
+                    if (TOOL_MATERIAL_STAGES.containsKey(material.identifier) && !GameStageHelper.hasAnyOf(player, stageData, TOOL_MATERIAL_STAGES.get(material.identifier))) {
+                        
                         player.sendMessage(new TextComponentString("You dropped the " + stack.getDisplayName() + "! Further progression is needed to wield " + material.getLocalizedName() + " tools."));
                         player.dropItem(true);
                         return;
                     }
                 }
-
+                
                 // Specific modifier prevention
                 for (final IModifier modifier : TinkerUtil.getModifiers(stack)) {
-
-                    if (TOOL_MODIFIER_STAGES.containsKey(modifier.getIdentifier()) && !stageData.hasUnlockedAnyOf(TOOL_MODIFIER_STAGES.get(modifier.getIdentifier()))) {
-
+                    
+                    if (TOOL_MODIFIER_STAGES.containsKey(modifier.getIdentifier()) && !GameStageHelper.hasAnyOf(player, stageData, TOOL_MODIFIER_STAGES.get(modifier.getIdentifier()))) {
+                        
                         player.sendMessage(new TextComponentString("You dropped the " + stack.getDisplayName() + "! Further progression is needed to use the " + modifier.getLocalizedName() + " modifier."));
                         player.dropItem(true);
                         return;
@@ -270,12 +232,5 @@ public class TinkerStages {
                 }
             }
         }
-    }
-    
-    
-    @EventHandler
-    public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
-        
-        LOG.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
     }
 }
--- src/main/java/net/darkhax/tinkerstages/compat/crt/TinkerStagesCrT.java
+++ src/main/java/net/darkhax/tinkerstages/compat/crt/TinkerStagesCrT.java
@@ -8,46 +8,46 @@ import stanhebben.zenscript.annotations.ZenMethod;
 @ZenRegister
 @ZenClass("mods.TinkerStages")
 public class TinkerStagesCrT {
-
+    
     @ZenMethod
     public static void addGeneralCraftingStage (String stage) {
-
+        
         TinkerStages.GENERAL_CRAFTING_STAGES.add(stage);
     }
-
+    
     @ZenMethod
     public static void addGeneralPartReplacingStage (String stage) {
-
+        
         TinkerStages.GENERAL_PART_REPLACING_STAGES.add(stage);
     }
-
+    
     @ZenMethod
     public static void addGeneralPartBuildingStage (String stage) {
-
+        
         TinkerStages.GENERAL_PART_BUILDING_STAGES.add(stage);
     }
-
+    
     @ZenMethod
     public static void addGeneralModifierStage (String stage) {
-
+        
         TinkerStages.GENERAL_MODIFIER_STAGES.add(stage);
     }
-
+    
     @ZenMethod
     public static void addToolTypeStage (String stage, String toolType) {
-
+        
         TinkerStages.TOOL_CRAFTING_STAGES.put(toolType, stage);
     }
-
+    
     @ZenMethod
     public static void addMaterialStage (String stage, String material) {
-
+        
         TinkerStages.TOOL_MATERIAL_STAGES.put(material, stage);
     }
-
+    
     @ZenMethod
     public static void addModifierStage (String stage, String modifier) {
-
+        
         TinkerStages.TOOL_MODIFIER_STAGES.put(modifier, stage);
     }
 }
